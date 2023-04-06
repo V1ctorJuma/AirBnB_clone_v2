4dcc704584e9b21ee165d907c16a2f370c1b71dc
V1ctorJuma <jumavictor151@gmail.com>
2023-04-06 18:00:14 +0000

    AirBnB_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
+Created on Mon Apr 6 14:58:05 2023
+@author: Victor Juma
 """
 from fabric.api import local, env
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+env.hosts = ['100.26.20.5', '34.239.255.144']
 
 
 def do_pack():
